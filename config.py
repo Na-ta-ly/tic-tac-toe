175d5e7263aab16d3f88b0d99b2385bd4672d442
Na-ta-ly <zolotarevanata@yandex.ru>
2023-09-25 17:35:45 +0300

    build!: use arcade library

--- config.py
+++ config.py
@@ -1,16 +1,39 @@
+BUTTONS_NAMES = ['Small game', 'Big game', 'Quit']
+
 BACKGROUND = (200, 200, 200)
-GREEDCOLOUR = (30, 30, 30)
+GRIDCOLOUR = (30, 30, 30)
 CELLCOLOUR = (220, 220, 220)
 BUTTONCOLOUR = (180, 180, 180)
 TEXTCOLOUR = (0, 0, 0)
 
-screen_dim = (880, 800)
-line_thickness = 3
+screen_dim = (800, 900)
+line_thickness = 2
 menu_height = 0.1
-menu_font_size = 45
+# menu_font_size = 45
 footer_height = 0.05
-state_bar_font_size = 15
-marks_size = 0.8
+# state_bar_font_size = 15
 
 small_game_size = 3
 big_game_size = 10
+
+x_pic = './images/x.png'
+o_pic = './images/o.png'
+button_width = 200
+button_space = 20
+
+style = dict()
+style['font_size'] = 22
+style['border_width'] = 1
+# style['vmargin'] = 40
+
+style['bg_color'] = BUTTONCOLOUR
+style['border_color'] = (0, 0, 0)
+style['font_color'] = (0, 0, 0)
+
+# style['bg_color_hover'] = BACKGROUND
+# style['border_color_hover'] = (0,0,0)
+# style['font_color_hover'] = (0,0,0)
+#
+# style['bg_color_press'] = BACKGROUND
+# style['border_color_press'] = (0,0,0)
+# style['font_color_press'] = (0,0,0)
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import logging
-from game_classes.player import Player
-from game_classes.game import Game
-from run_game import run_game
+import arcade
+from interface_classes.interface import Interface
+
 
 if __name__ == '__main__':
 
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s',
-        level=logging.DEBUG
+        level=logging.INFO
     )
 
-    player_1 = Player('Игрок 1')
-    player_2 = Player('Игрок 2')
-    game = Game([player_1, player_2])
-    run_game(10, game)
+    interface = Interface("Tic-Tac-Toe")
+    interface.setup(10)
+    arcade.run()
